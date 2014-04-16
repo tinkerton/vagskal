a5ca2849c45e388b508b3a0b39454dc803269325
carolinadahlberg <carolina.dahlberg@gmail.com>
2014-04-16 12:55:53 +0200

    .Js filer Maria
    Nya js filer uppdaterade av Jonas.

--- js/CaseMaria_Mail.js
+++ js/CaseMaria_Mail.js
@@ -7,17 +7,35 @@ self.preload = {"images":
 
 self.nodes = {"content": 
 	[
-	
+		
 	{  	"ID": "SM1.10",
-		"type":"info",
-		"background":{"type":"image","url":"bg.jpg"},
-		"pretext":"Detta är Marias mail-case",
-		"justify":"leftifmobile",
-     	"animation":"fade",
-		"showNextButton":"0",
-		"callback":"Case1a",
-		"callbackNode":"4"
+	"type":"info",
+	"background":{"type":"image","url":"bg.jpg"},
+	"pretext":"Detta är Marias mail-case",
+	"image": [
+	{"url":"ansokan_maria.png"} 
+	],
+	"justify":"leftifmobile",
+    "animation":"fade",
+	"showNextButton":"0"
+	},
+		{  	"ID": "SM1.11",
+	"type":"video_seq",
+	"background":{"type":"image","url":"bg.jpg"},
+	"sequences":[
+	{
+	"sequenceID":"0",
+	"type":"video",
+	"url":"http://player.vimeo.com/video/91730826"
+	}],
+	"animation":"fade",
+	"showNextButton":"0",
+	"callback":"Case1a",
+	"callbackNode":"4"
 	}
+
+
+	
 	
 ]};
 
--- js/CaseMaria_Video.js
+++ js/CaseMaria_Video.js
@@ -8,16 +8,58 @@ self.preload = {"images":
 self.nodes = {"content": 
 	[
 	
-	{  	"ID": "SI1.10",
-		"type":"info",
+	{  	"ID": "MV1.10",
+		"type":"video_seq",
 		"background":{"type":"image","url":"bg.jpg"},
-		"pretext":"Detta är Marias Video-case",
-		"justify":"leftifmobile",
+		"sequences":[
+		{
+				"sequenceID":"0",
+				"type":"question",
+				"text":"Fråga Maria:",
+				"answers":[
+							{"text":"Berätta lite om dig själv", "gotoID":"1"}, 
+							{"text":"Varför sökte du det här jobbet?", "gotoID":"2"},  
+							{"text":"Vad är dina främsta styrkor?", "gotoID":"3"},
+							{"text":"Vad är dina svagheter?", "gotoID":"4"} ,
+							{"text":"Avsluta intervjun", "gotoID":"-1"} 
+						]
+					},
+							{	
+								"sequenceID":"1",
+								"type":"video",
+							 	"url":"http://player.vimeo.com/video/91707282", 
+							 	 "gotoID":"0"
+							},
+							{	
+								"sequenceID":"2",
+								"type":"video",
+								"url":"http://player.vimeo.com/video/91707335", 
+								 "gotoID":"0"
+							},
+
+							{	
+								"sequenceID":"3",
+								"type":"video",
+								"url":"http://player.vimeo.com/video/91707389", 
+								 "gotoID":"0"
+							},
+							{	
+								"sequenceID":"4",
+								"type":"video",
+								"url":"http://player.vimeo.com/video/91707389", 
+								 "gotoID":"0"
+							}
+			],
      	"animation":"fade",
-		"showNextButton":"0",
+		"showNextButton":"-1",
 		"callback":"Case1a",
 		"callbackNode":"4"
 	}
+
+	
+
+
+	
 	
 ]};
 
